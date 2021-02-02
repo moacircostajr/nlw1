7630e7937a68a38ab57bfae9276b348148edaf0d
Moacir Costa <moacircostajr@gmail.com>
2021-02-02 02:47:45 -0300

    query param

--- server/src/server.ts
+++ server/src/server.ts
@@ -5,11 +5,29 @@ const app = express()
 // Rota: Endereço completo da requisição
 // Recurso: Qual entidade estamos acessando do sistema
 
+// Request Param: Parâmetros que vem na própria rota que identificam um recurso
+// Query Param: Parâmetros que vêm na própria rota, opcionais, separados por ponto de interrogação, usados para filtragem e/ou paginação
+
+const users = [
+  'Diego',
+  'Cleiton',
+  'Robson',
+  'Daniel'
+]
+
 app.get('/users', (request, response) => {
-  console.log('Listagem de usuários')
+  const search = String(request.query.search)
+  console.log(search)
+  const filteredUsers = search ? users.filter(user => user.includes(search)) : users
   // response.send('Hello World!')
   // JSON
-  return response.json(['Diego', 'Cleiton', 'Robson', 'Moacir'])
+  return response.json(filteredUsers)
+})
+
+app.get('/users/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const user = users[id]
+  return response.json(user)
 })
 
 app.post('/users', (request, response) => {
