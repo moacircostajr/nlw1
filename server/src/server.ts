b233c25456939b101a13d54ee7d0405d717c8de9
Moacir Costa <moacircostajr@gmail.com>
2021-02-02 02:03:12 -0300

    requisições get e post

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,10 +2,22 @@ import express from 'express'
 
 const app = express()
 
+// Rota: Endereço completo da requisição
+// Recurso: Qual entidade estamos acessando do sistema
+
 app.get('/users', (request, response) => {
   console.log('Listagem de usuários')
   // response.send('Hello World!')
-  response.json(['Diego', 'Cleiton', 'Robson', 'Moacir'])
+  // JSON
+  return response.json(['Diego', 'Cleiton', 'Robson', 'Moacir'])
+})
+
+app.post('/users', (request, response) => {
+  const user = {
+    name: 'Diego',
+    email: 'diego@rocketseat.com.br'
+  }
+  return response.json(user)
 })
 
 app.listen(3333)
