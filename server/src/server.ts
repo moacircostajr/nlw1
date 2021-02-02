5f80a770e37d68dc8408404c86a2c13e2e2b7c01
Moacir Costa <moacircostajr@gmail.com>
2021-02-02 03:11:41 -0300

    desacopla arquivo de rotas

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,10 +1,8 @@
 import express from 'express'
+import routes from './routes' // o ./ não é necessário quando o arquivo a ser importado está dentro da pasta node_modules
 
 const app = express()
 app.use(express.json())
-
-app.get('/', (request, response) => {
-  return response.json({ message: "Hello World" })
-})
+app.use(routes)
 
 app.listen(3333)
