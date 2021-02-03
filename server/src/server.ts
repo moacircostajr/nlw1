be49cb1a99e621e73140cdf20d9e7a8749cdcf10
Moacir Costa <moacircostajr@gmail.com>
2021-02-03 03:04:51 -0300

    carrega arquivos de imagens da pasta uploads

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,8 +1,11 @@
 import express from 'express'
+import path from 'path'
 import routes from './routes' // o ./ não é necessário quando o arquivo a ser importado está dentro da pasta node_modules
 
 const app = express()
 app.use(express.json())
 app.use(routes)
+
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')))
 
 app.listen(3333)
