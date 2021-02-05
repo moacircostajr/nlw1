9ce81f01d3de42013132752be0835dc38a9d99ec
Moacir Costa <moacircostajr@gmail.com>
2021-02-05 06:00:45 -0300

    conclui homepage

---
+++ web/src/pages/CreatePoint/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+interface Props {
+
+}
+
+const CreatePoint = (props: Props) => {
+  return (
+    <div>
+      <h1>Create Point</h1>
+    </div>
+  )
+}
+
+export default CreatePoint
--- web/src/pages/Home/index.tsx
+++ web/src/pages/Home/index.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import logo from '../../assets/logo.svg'
 import './styles.css'
 import { FiLogIn } from 'react-icons/fi'
+import { Link } from 'react-router-dom'
 
 interface Props {
 
@@ -19,10 +20,10 @@ const Home = (props: Props) => {
             <h1>Seu marketplace de coleta de resíduos.</h1>
             <p>Ajudamos pessoas a encontrarem pontos de coleta de forma eficiente</p>
 
-            <a href="/cadastro">
+            <Link to="/create-point">
               <span><FiLogIn /></span>
               <strong>Cadastre um ponto de coleta</strong>
-            </a>
+            </Link>
           </main>
         </div>
       </div>
